83c5f85ce32e6610d1ad46c3ca35b1ed83d17740
Harrison Schaen <hschaen@gmail.com>
2020-04-29 15:50:39 -0700

    Updated script.js to ES6
    changed local variables to let, changed function to ES6 syntax

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+const writePassword = () => {
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
